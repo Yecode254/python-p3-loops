23ec2353a0275f8e19a240c193e69f5a1822e71e
Yego <eddieyego273@gmail.com>
2025-05-18 13:53:06 +0300

    Complete

--- lib/looping.py
+++ lib/looping.py
@@ -8,11 +8,21 @@ def happy_new_year():
         counter -= 1
     print ("Happy New Year!")
 
-
 def square_integers(int_list):
     # code goes here!
-    pass
+    square_ints = [int * int for int in int_list]
+    return square_ints
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for num in range(1,101):
+        if (num % 3 == 0 and num % 5 == 0):
+            print ("FizzBuzz")
+
+        elif num % 3 == 0:
+            print ("Fizz")
+        
+        elif num % 5 == 0:
+            print ("Buzz")
+        else:
+            print (num )
